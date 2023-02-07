f03fb60c87c086fb5778fc67a34c6666263b80f5
Jack Preston <je.preston20@gmail.com>
2023-02-07 11:15:31 +0000

    feat: add Adder component and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,92 @@
 import './App.css';
+import { useState } from 'react';
+
+function ListItem({number, index, deleteItem}) {
+  return (
+    <div className='flex-parent list-item'>
+      <div className='flex-grow'>
+        {number}
+      </div>
+      <div className='flex-shrink'>
+        <button className='delete-button' onClick={() => deleteItem(index)}>x</button>
+      </div>
+    </div>
+  )
+}
+
+function AddNumber({inputNumber, handleChange, handleSubmit}) {
+  return (
+    <form className='flex-parent' onSubmit={handleSubmit}>
+      <input 
+        type='number' 
+        id='inputNumber' 
+        name='number' 
+        value={inputNumber} 
+        onChange={handleChange}
+        className='flex-grow'/>
+      <button type='submit' className='add-button flex-shrink'>Add</button>
+    </form>
+  )
+}
+
+function Adder() {
+  const [numbers, setNumbers] = useState([]);
+  const [inputNumber, setInputNumber] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newNumber = Number(e.target.number.value);
+    setNumbers([
+      ...numbers,
+      newNumber
+    ]);
+
+    setInputNumber('');
+  }
+
+  function handleChange(e) {
+    setInputNumber(e.target.value);
+  }
+
+  function deleteItem(index) {
+    const newNumbers = numbers.slice();
+    newNumbers.splice(index, 1);
+    setNumbers([...newNumbers]);
+  }
+
+  function sumNumbers(numbers) {
+    return numbers.reduce((sum, currentValue) => sum + currentValue, 0);
+  }
+
+  return (
+    <div className='adder'>
+      <AddNumber
+        inputNumber={inputNumber}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <div>
+        {numbers.map((number, index) => {
+          return <ListItem
+            key={index}
+            number={number}
+            index={index}
+            deleteItem={deleteItem}
+          />
+        })}
+      </div>
+      <div className='sum'>
+        Sum: {sumNumbers(numbers)}
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      <Adder/>
     </div>
   );
 }
